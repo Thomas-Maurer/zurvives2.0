5387b33143edfa8510aaa6f1f712332315c95a34
Thomas Maurer <toto-ange@hotmail.fr>
2015-10-29 14:20:47 +0100

    Init monster

---
+++ api/controllers/MonsterController.js
@@ -0,0 +1,11 @@
+/**
+ * MonsterController
+ *
+ * @description :: Server-side logic for managing monsters
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
