4892c9144b00ff3b0202ada3cfd015e0f7bcb0a9
Thomas Maurer <toto-ange@hotmail.fr>
2015-10-29 14:14:58 +0100

    Init controller Talent

---
+++ api/controllers/TalentController.js
@@ -0,0 +1,11 @@
+/**
+ * TalentController
+ *
+ * @description :: Server-side logic for managing talents
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
